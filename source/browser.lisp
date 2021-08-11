bdcdfdf9f7bd9cd3e433db3cf34cd9ca4e86c4ce
Artyom Bologov <aartaka@protonmail.com>
2021-08-11 12:19:12 +0500

    Handle extension messages (still broken on asynchronicity!).

--- source/browser.lisp
+++ source/browser.lisp
@@ -718,4 +718,4 @@ sometimes yields the wrong result."
 (define-ffi-generic ffi-buffer-cookie-policy (buffer value))
 (define-ffi-generic ffi-set-preferred-languages (buffer value))
 (define-ffi-generic ffi-focused-p (buffer))
-(define-ffi-generic ffi-buffer-send-message (buffer &key name contents when-loaded-p wait-p))
+(define-ffi-generic ffi-buffer-send-message (buffer &key name contents when-loaded-p))
